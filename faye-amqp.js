021a0485896da5a41a70d608a62fda576ff5c163
Jonathan Griggs <jonathan.griggs@gmail.com>
2012-03-02 08:29:29 -0700

    Implemented channel mapping / tests

--- faye-amqp.js
+++ faye-amqp.js
@@ -1,10 +1,20 @@
 var Engine = function(server, options) {
-	this._server  = server;
-	this._options = options || {};
-	
-	this._clients   = {};
-	this._channels  = {};
-  	this._messages  = {};
+    this._server  = server;
+    console.log(this._server);
+    this._options = options || {};
+
+    this._clients   = {};
+    this._channels  = {};
+    this._messages  = {};
+    this._queues = {};
+
+    host   = this._options.host     || 'localhost',
+    port   = this._options.port     || '5672',
+    this._exchange = this._options.exchange || 'amq.topic',
+    user   = this._options.user,
+    auth   = this._options.password;
+
+    this._conn;
 
 };
 
@@ -14,23 +24,52 @@ Engine.create = function(server, options) {
 
 Engine.prototype = {
   createClient: function(callback, context) {
-    //var clientId = this._namespace.generate();
-    var clientId = this._server.generateId(), self = this;
-    self._server.debug('Created new client ?', clientId);
-    self.ping(clientId);
-    self._server.trigger('handshake', clientId);
-    callback.call(context, clientId);
+    
+    this.getAmqpClient(function(conn) {
+        console.log("====Connection======");
+        console.log(conn);
+        self = this;
+        var q = conn.queue('',function (queue) {
+                console.log("====QUEUE======");
+                console.log(queue);
+                queue.subscribe(function (message, headers, deliveryInfo) {
+                    self._server.debug('Received message ? to channel ?', this.name, deliveryInfo.routingKey);
+                });
+                var clientId = queue.name;
+                self._server.debug('Created new client ?', clientId);
+                self.ping(clientId);
+                self._server.trigger('handshake', clientId);
+                callback.call(context, clientId);
+        });
+    });    
+  },
+  
+  getAmqpClient: function(callback) {
+    if(typeof this._conn === "undefined") {
+        var amqp = require('amqp');
+        try {
+        this._conn = amqp.createConnection(
+            {url: "amqp://" + host + ":" + port, defaultExchangeName: this._exchange}).
+            on('ready', function() {
+                callback(this);
+            });
+        } catch(error) {
+            console.log(error);
+        }
+    } else {
+        callback(this._conn);
+    }
   },
   
   destroyClient: function(clientId, callback, context) {
-    if (!this._namespace.exists(clientId)) return;
+    //if (!this._namespace.exists(clientId)) return;
     var clients = this._clients;
     
     if (clients[clientId])
       clients[clientId].forEach(function(channel) { this.unsubscribe(clientId, channel) }, this);
     
     this.removeTimeout(clientId);
-    this._namespace.release(clientId);
+    //this._namespace.release(clientId);
     delete this._messages[clientId];
     this._server.debug('Destroyed client ?', clientId);
     this._server.trigger('disconnect', clientId);
@@ -46,10 +85,12 @@ Engine.prototype = {
     if (typeof timeout !== 'number') return;
     
     this._server.debug('Ping ?, ?', clientId, timeout);
+    /*
     this.removeTimeout(clientId);
     this.addTimeout(clientId, 2 * timeout, function() {
       this.destroyClient(clientId);
     }, this);
+    */
   },
   
   subscribe: function(clientId, channel, callback, context) {
@@ -61,6 +102,13 @@ Engine.prototype = {
     
     channels[channel] = channels[channel] || new Faye.Set();
     channels[channel].add(clientId);
+    
+    for (var i = 0, n = queues.length; i < n; i++) {
+        var queue = queues[i];
+        if(queue.name == clientId) {
+            queue.bind(this._exchange, channel);
+        }
+    }
     
     this._server.debug('Subscribed client ? to channel ?', clientId, channel);
     if (trigger) this._server.trigger('subscribe', clientId, channel);
@@ -80,6 +128,13 @@ Engine.prototype = {
     if (channels[channel]) {
       channels[channel].remove(clientId);
       if (channels[channel].isEmpty()) delete channels[channel];
+    }
+    
+    for (var i = 0, n = queues.length; i < n; i++) {
+        var queue = queues[i];
+        if(queue.name == clientId) {
+            queue.unbind(this._exchange, channel);
+        }
     }
     
     this._server.debug('Unsubscribed client ? from channel ?', clientId, channel);
@@ -114,6 +169,19 @@ Engine.prototype = {
     if (!this._server.hasConnection(clientId)) return;
     this._server.deliver(clientId, this._messages[clientId]);
     delete this._messages[clientId];
+  },
+  
+  // Helper methods specific to AMQP Engine
+  toAmqpChannel: function(bayeuxChannel) {
+    if(typeof bayeuxChannel !== "string") return;
+    // Remove the leading slash
+    if(bayeuxChannel.charAt(0) === '/') bayeuxChannel = bayeuxChannel.slice(1);
+    
+    bayeuxChannel = bayeuxChannel.replace('/','.');
+    bayeuxChannel = bayeuxChannel.replace('**','*.#');
+    return bayeuxChannel
   }
+  
 };
+//Faye.extend(Engine.prototype, Faye.Timeouts);
 module.exports = Engine;
--- spec/faye_amqp_spec.js
+++ spec/faye_amqp_spec.js
@@ -1,30 +1,59 @@
 var AmqpEngine = require('../faye-amqp')
 
 JS.ENV.FayeAmqpSpec = JS.Test.describe("AMQP engine", function() { with(this) {
-  before(function() {
-    var pw = process.env.TRAVIS ? undefined : "foobared"
-    this.engineOpts = {type: AmqpEngine}
-  })
-  
-  after(function(resume) { with(this) {
-    sync(function() {
-      engine.disconnect()
-      //var amqp = amqp.createConnection({url:localhost});
-      //
-      //redis.auth(engineOpts.password)
-      //redis.flushall(function() {
-      //  redis.end()
-      //  resume()
-      //})
+
+    before(function() {
+        var pw = process.env.TRAVIS ? undefined : "foobared"
+        this.engineOpts = {type: AmqpEngine}
     })
-  }})
-  
-  itShouldBehaveLike("faye engine")
-  
-  describe("distribution", function() { with(this) {
-    itShouldBehaveLike("distributed engine")
-  }})
-  
-  if (process.env.TRAVIS) return
-  
+
+    after(function(resume) { with(this) {
+
+        sync(function() {
+            /*
+            engine.disconnect()
+            
+            var amqp = amqp.createConnection({url:localhost});
+
+            redis.auth(engineOpts.password)
+            redis.flushall(function() {
+            redis.end()
+            resume()
+            */
+        })
+
+    }})
+
+    it("translates Bayeux channels to AMQP channels", function() { with(this) {
+        var engine = new AmqpEngine();
+        var bayeuxChannel = "/foo";
+        var amqpChannel = "foo";
+        assertEqual(amqpChannel,engine.toAmqpChannel(bayeuxChannel));
+
+        bayeuxChannel = "/foo/*";
+        amqpChannel = "foo.*";
+        assertEqual(amqpChannel,engine.toAmqpChannel(bayeuxChannel));
+
+        bayeuxChannel = "/foo/**";
+        amqpChannel = "foo.*.#";
+        assertEqual(amqpChannel,engine.toAmqpChannel(bayeuxChannel));
+
+        bayeuxChannel = "/foo/bar";
+        amqpChannel = "foo.bar";
+        assertEqual(amqpChannel,engine.toAmqpChannel(bayeuxChannel));
+
+        bayeuxChannel = "/foo";
+        amqpChannel = "foo";
+        assertEqual(amqpChannel,engine.toAmqpChannel(bayeuxChannel));
+    }});
+
+    
+    itShouldBehaveLike("faye engine")
+
+    describe("distribution", function() { with(this) {
+        itShouldBehaveLike("distributed engine")
+    }})
+
+    if (process.env.TRAVIS) return
+    
 }})
